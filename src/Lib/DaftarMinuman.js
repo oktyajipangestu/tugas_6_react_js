33d300e83a040cfe12d8c2688598362125fcecd4
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-03 23:28:09 +0700

    menambahkan menu kontakmakanan dan minuman (selesai)

---
+++ src/Lib/DaftarMinuman.js
@@ -0,0 +1,30 @@
+const DaftarMinuman = [
+    {
+        NamaMinuman: "Es Cendol",
+        Harga: "10.000",
+        img: "https://www.goodnewsfromindonesia.id/uploads/post/large-es-cendol-dawet-d85dbbfe4b7506f2ef180406b969ff2d.jpg"
+    },
+    {
+        NamaMinuman: "Es Pisang Hijau",
+        Harga: "10.000",
+        img: "https://cdn0-production-images-kly.akamaized.net/v-UQfG2df0rlAQuayjSb02-p5xw=/1x104:1000x667/469x260/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3403265/original/085003600_1615950875-shutterstock_1814382314.jpg"
+    },
+    {
+        NamaMinuman: "Es Kacang Merah",
+        Harga: "10.000",
+        img: "https://www.resepkuerenyah.com/wp-content/uploads/2016/11/28.-resep-es-kacang-merah-1024x683.jpg"
+    },
+    {
+        NamaMinuman: "Es Pleret",
+        Harga: "10.000",
+        img: "https://joss.co.id/data/uploads/2019/03/es-gempol-pleret.jpg"
+    },
+    {
+        NamaMinuman: "Es Legen",
+        Harga: "10.000",
+        img: "https://3.bp.blogspot.com/-0P4sHZlgc6o/V72NIndHg-I/AAAAAAAAEgk/Ip-KmeLm7Js6gRJ8SpNHZDWyTowSHStnwCPcB/s1600/maxresdefault.jpg"
+    }
+
+];
+
+export default DaftarMinuman;
--- src/Page/MenuMinuman.js
+++ src/Page/MenuMinuman.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import Header from "./Header";
+import DaftarMinuman from "../Lib/DaftarMinuman";
 
 class MenuMinuman extends Component {
     render() {
         return (
             <div>
-
+                <Header />
+                <h3>Daftar Minuman favourite</h3>
+                <table style={{ width: "100%" }}>
+                    <tbody>
+                        <tr>
+                            {DaftarMinuman.map((data, index) => {
+                                return (
+                                    <td key={index}>
+                                        <center>
+                                            <img src={data.img} alt="Product Makanan" width="150" height="100"/>
+                                            <p>{data.NamaMinuman}</p>
+                                            <p>Harga : Rp. {data.Harga}</p>
+                                        </center>
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
